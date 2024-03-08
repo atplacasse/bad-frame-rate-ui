65b4592640bd059712b8426d54c52adb73801cd6
Patrick Lacasse <placasse@arcanetech.io>
2024-03-08 10:13:01 -0500

    A huge model to drop fps.

---
+++ Assets/Controller.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Arcane
+{
+    public class Controller : MonoBehaviour
+    {
+        [SerializeField]
+        Slider mainSlider;
+
+        [SerializeField]
+        Button cursorModeButton;
+
+        [SerializeField]
+        Texture2D handCursor;
+
+        [SerializeField]
+        CursorMode cursorMode = CursorMode.Auto;
+
+        Vector3 originalCameraPosition;
+
+        public void Start()
+        {
+            originalCameraPosition = Camera.main.transform.position;
+
+            mainSlider.onValueChanged.AddListener(delegate { OnMainSliderValueChanged(); });
+            cursorModeButton.onClick.AddListener(delegate { OnCursorModeButtonClicked(); });
+
+            SetCursor();
+        }
+
+        void OnCursorModeButtonClicked()
+        {
+            cursorMode = cursorMode == CursorMode.Auto ? CursorMode.ForceSoftware : CursorMode.Auto;
+            SetCursor();
+        }
+
+        void SetCursor()
+        {
+            var hotspot = new Vector2(0.35f * handCursor.width, 0 * handCursor.height);
+            Cursor.SetCursor(handCursor, hotspot, cursorMode);
+
+            cursorModeButton.GetComponentInChildren<Text>().text = $"Current cursor mode: {cursorMode}";
+        }
+
+        void OnMainSliderValueChanged()
+        {
+            var newCameraPosition = originalCameraPosition;
+            newCameraPosition.x += mainSlider.value;
+            Camera.main.transform.position = newCameraPosition;
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-Bad Frame Rate UI
+# Bad Frame Rate UI
 
 This project has too much assets in its scene which causes performance issues and bad framerate.
 It also have a UI which is affected by the performance issue.
@@ -6,3 +6,6 @@ It also have a UI which is affected by the performance issue.
 The goal of the game is to fixe the performance issue of the UI.
 UI should be fluid.
 There is one rule: the main 3D scene (everything except canvas) cannot be fixed.
+
+The project was created with Unity 2018, but this is not a constraint and any solution involving upgrading to a recent Unity version would still be appreciated.
+
